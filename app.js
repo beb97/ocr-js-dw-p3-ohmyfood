2be768cae168a9d62d0afed6533a15ffd14fc969
beb97 <beb97@hotmail.com>
2021-05-30 11:29:14 +0200

    ajout menu 2 / 3 / 4 automatisation des menus en JS

---
+++ app.js
@@ -0,0 +1,27 @@
+const currentMenuId = new URLSearchParams(window.location.search).get("menu");
+const currentMenu = menus[currentMenuId];
+
+// Tab tittle
+document.title=currentMenu.nom;
+// Image
+document.querySelector("img").setAttribute("src", currentMenu.image);
+// Menu name
+document.querySelector("h2").innerHTML = currentMenu.nom;
+// Dishes
+append(currentMenu.entrees, "#entrees");
+append(currentMenu.plats, "#plats");
+append(currentMenu.desserts, "#desserts");
+
+
+function append(array, anchor) {
+    let temp = document.querySelector("#menu-template");
+
+    array.forEach(entry => {
+        let clone = temp.cloneNode(true);
+        clone.removeAttribute('id');
+        clone.querySelector(".item-text_title").innerHTML=entry.name;
+        clone.querySelector(".item-text_desc").innerHTML=entry.desc;
+        clone.querySelector(".item-price").innerHTML=entry.price;
+        document.querySelector(anchor).appendChild(clone);
+    })
+}
--- menu.js
+++ menu.js
@@ -1,69 +1,80 @@
-let entrees =
-    [{
-        "name":"Fricassée d'escargot",
-        "desc":"Au piment d'Espelette ",
-        "price":"25€"
+let menus = {
+    "1": {
+        "nom":"La palette du goût",
+        "image":"img/1-palette.jpg",
+        entrees:[
+            {"name":"Fricassée d'escargot", "desc":"Au piment d'Espelette", "price":"25€"},
+            {"name":"Foie gras de canard mi-cuit", "desc":"Et ses copeaux de truffe noire", "price":"35€"},
+            {"name":"Oeuf au plat", "desc":"Assaisonné à la truffe sur lit de caviar", "price":"20€"}
+        ],
+        plats:[
+            {"name":"Filet de boeuf aux herbes", "desc":"Accompagné de sa ribambelle de légumes", "price":"40€"},
+            {"name":"Parmentier de queue de boeuf", "desc":"À la truffe noire sur sa purée de panais", "price":"35€"},
+            {"name":"Filet de turbot", "desc":"Aux agrumes", "price":"44€"}
+        ],
+        desserts: [
+            {"name":"Paris-Brest", "desc":"Revisité", "price":"18€"},
+            {"name":"Macaron au chocolat d'exception ", "desc":"Et glace à la vanille de Madagascar", "price":"22€"},
+            {"name":"Mousse au chocolat ", "desc":"Au piment d'Espelette et à la truffe noire", "price":"23€"}
+        ]
     },
-    {
-        "name":"Foie gras de canard mi-cuit",
-        "desc":"Et ses copeaux de truffe noire",
-        "price":"35€"
+    "2": {
+        "nom":"La note enchantée",
+        "image":"img/2-note.jpg",
+        entrees:[
+            {"name":"Ravioles de foie gras ", "desc":"Accompagnés de leur crème à la truffe", "price":"25€"},
+            {"name":"Caviar osciètre ", "desc":"Sur blini à la farine de blé noir", "price":"35€"},
+            {"name":"Homard et espuma de potiron", "desc":"Mariné aux zestes d'orange", "price":"20€"},
+            {"name":"Foie gras de canard cuit entier", "desc":"Confiture de figue et pain toasté", "price":"35€"}
+        ],
+        plats:[
+            {"name":"Noix de coquilles Saint-Jacques", "desc":"Sur lit de purée de céleri-rave", "price":"40€"},
+            {"name":"Langoustine poêlée", "desc":"Purée de patate douce", "price":"35€"},
+            {"name":"Mijoté de queue de boeuf ", "desc":"Et riz sauvage aux zestes de citron", "price":"44€"},
+        ],
+        desserts: [
+            {"name":"Macaron noisette et chocolat", "desc":"Glace au caramel brun et sel de Guérande", "price":"18€"},
+            {"name":"Baba au rhum revisité ", "desc":"Avec son coulis de citron", "price":"22€"},
+            {"name":"Tarte au citron meringuée ", "desc":"Déstructurée", "price":"23€"},
+        ]
     },
-    {
-        "name":"Oeuf au plat",
-        "desc":"Assaisonné à la truffe sur lit de caviar",
-        "price":"20€"
-    }]
-
-let plats=
-    [{
-        "name":"Filet de boeuf aux herbes",
-        "desc":"Accompagné de sa ribambelle de légumes",
-        "price":"40€"
+    "3":{
+        "nom":"À la française",
+        "image":"img/3-francaise.jpg",
+        entrees:[
+            { "name":"Fricassée d'escargot", "desc":"Au piment d'Espelette ", "price":"25€"},
+            {"name":"Foie gras de canard mi-cuit", "desc":"Et ses copeaux de truffe noire", "price":"35€"},
+            {"name":"Oeuf au plat", "desc":"Assaisonné à la truffe sur lit de caviar", "price":"20€" }
+        ],
+        plats:[
+            {"name":"Filet de boeuf aux herbes", "desc":"Accompagné de sa ribambelle de légumes", "price":"40€"},
+            {"name":"Parmentier de queue de boeuf", "desc":"À la truffe noire sur sa purée de panais", "price":"35€" },
+            {"name":"Filet de turbot", "desc":"Aux agrumes", "price":"44€"}
+        ],
+        desserts: [
+            {"name":"Paris-Brest", "desc":"Revisité", "price":"18€"},
+            {"name":"Macaron au chocolat d'exception", "desc":"Et glace à a vanille de Madagascar", "price":"22€"},
+            {"name":"Mousse au chocolat", "desc":"Au piment d'Espelette tà la truffe noire", "price":"23€"}
+        ]
     },
-    {
-        "name":"Parmentier de queue de boeuf",
-        "desc":"À la truffe noire sur sa purée de panais",
-        "price":"35€"
+    "4": {
+        "nom":"Le délice des sens",
+        "image":"img/4-delice.jpg",
+        entrees:[
+            {"name":"Tartare de thon", "desc":"Assaisonné au yuzu", "price":"25€"},
+            {"name":"Bouchée de homard croustillant ", "desc":"Et sa farandole de petits légumes", "price":"35€"},
+            {"name":"Velouté de cèpes ", "desc":"Aux truffes", "price":"20€"},
+        ],
+        plats:[
+            {"name":"Poulet rôti aux herbes de Provence ", "desc":"Et sa crème de truffe", "price":"40€"},
+            {"name":"Langouste rôtie ", "desc":"Et ses légumes de saison", "price":"35€"},
+            {"name":"Côte de boeuf Angus ", "desc":"Et sa purée de panais", "price":"44€"},
+        ],
+        desserts: [
+            {"name":"Farandole de desserts", "desc":"Du chef", "price":"18€"},
+            {"name":"Crème brulée ", "desc":"Revisitée", "price":"22€"},
+            {"name":"Tiramisu ", "desc":"À la noisette", "price":"23€"},
+        ]
     },
-    {
-        "name":"Filet de turbot",
-        "desc":"Aux agrumes",
-        "price":"44€"
-    }]
-
-let desserts=
-    [{
-        "name":"Paris-Brest",
-        "desc":"Revisité",
-        "price":"18€"
-    },
-    {
-        "name":"Macaron au chocolat d'exception ",
-        "desc":"Et glace à la vanille de Madagascar",
-        "price":"22€"
-    },
-    {
-        "name":"Mousse au chocolat ",
-        "desc":"Au piment d'Espelette et à la truffe noire",
-        "price":"23€  "
-    }]
-
-
-append(entrees, "#entrees");
-append(plats, "#plats");
-append(desserts, "#desserts");
-
-function append(array, anchor) {
-
-    let temp = document.querySelector("#menu-template");
-
-    array.forEach(entry => {
-        let clone = temp.cloneNode(true);
-        clone.removeAttribute('id');
-        clone.querySelector(".item-text_title").innerHTML=entry.name;
-        clone.querySelector(".item-text_desc").innerHTML=entry.desc;
-        clone.querySelector(".item-price").innerHTML=entry.price;
-        document.querySelector(anchor).appendChild(clone);
-    })
 }
+
